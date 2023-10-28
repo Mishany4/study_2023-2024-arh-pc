da120d4034321780a869aba7b1ebef6f7443d907
Mishany4 <miskh4@gmail.com>
2023-10-29 00:18:16 +0300

    lab04

---
+++ labs/lab04/lab4.asm
@@ -0,0 +1,19 @@
+; hello.asm
+SECTION .data				; Начало секции данных
+    hello: 	DB 'Hello world! Меня зовут Ван Сихэм Франклин О Нил ДЖон',10	; 'Hello world!' плюс
+    					; символ перевода строки
+    helloLen:   EQU $-hello		; Длина строки hello
+    
+SECTION .text	      ; Начало секции кода
+    GLOBAL _start
+    
+_start:		      ; Точка входа в программу
+    mov eax,4	      ; Системый вызов для записи (sys_write)
+    mov ebx,1	      ; писатель файла '1' - стандартный вывод
+    mov ecx,hello     ; дрес строки hello в ecx
+    mov edx,helloLen  ; азмер строки hello
+    int 80h           ; Вызов ядра
+    
+     mov eax,1	      ; Системый вызов для вывода (sys_exit)
+     mov ebx,0	      ; Выход с кодом возврата '0' (без ошибок)
+     int 80h          ; Вызов ядра
